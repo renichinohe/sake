2681c41cb6a3d323dd2a2c351a5a1acd09d6ff1c
renichinohe <renichinohe@icloud.com>
2018-02-25 23:01:31 +0900

    gitignoreの更新

---
+++ src/main/java/modeling/sake/SakeApplication.java
@@ -0,0 +1,12 @@
+package modeling.sake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakeApplication.class, args);
+	}
+}
